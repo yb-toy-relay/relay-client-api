90567713a5df8f7485050a6b122b576dff2dfde6
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-20 14:05:36 +0900

    init

---
+++ src/main/java/one/appscale/relayclientapi/RelayClientApiApplication.java
@@ -0,0 +1,13 @@
+package one.appscale.relayclientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayClientApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelayClientApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/appscale/relayclientapi/RelayClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.appscale.relayclientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayClientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
