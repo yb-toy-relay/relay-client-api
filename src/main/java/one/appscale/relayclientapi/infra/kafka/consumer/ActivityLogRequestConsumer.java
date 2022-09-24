1d9ec5f7454da5d105d7b1b6dd67ef8dbf8f3be8
yaboong <daniel.yebon.yoo@gmail.com>
2022-09-25 06:09:42 +0900

    [RA-58] request -> produce kafka -> consumer -> s3

--- src/main/java/one/appscale/relayclientapi/infra/kafka/consumer/ActivityLogRequestConsumer.java
+++ src/main/java/one/appscale/relayclientapi/infra/kafka/consumer/ActivityLogRequestConsumer.java
@@ -3,23 +3,28 @@ package one.appscale.relayclientapi.infra.kafka.consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import one.appscale.relayclientapi.domain.activitylog.ActivityLogSearchQuery;
-import one.appscale.relayclientapi.domain.csv.CsvResource;
-import one.appscale.relayclientapi.domain.csv.CsvService;
+import one.appscale.relayclientapi.domain.csv.CsvResource2;
+import one.appscale.relayclientapi.domain.csv.CsvService2;
+import one.appscale.relayclientapi.infra.aws.s3.RelayS3Client;
 import one.appscale.relayclientapi.infra.kafka.producer.ActivityLogRequestProducer;
 import one.appscale.relayschema.request.csv.ActivityLogCsvRequest;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import static one.appscale.relayclientapi.infra.aws.s3.helper.ObjectMetadataFactory.csvNotifiableObjectMetadata;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ActivityLogRequestConsumer {
-    private final CsvService csvService;
+    private final CsvService2 csvService;
+    private final RelayS3Client relayS3Client;
 
     @KafkaListener(topics = ActivityLogRequestProducer.TOPIC,
                    containerFactory = "activityLogRequestConsumerFactory")
     public void consumeActivityLogCsvRequest(final ActivityLogCsvRequest request) {
         log.info("consume ActivityLogCsvRequest: {}", request);
-        final CsvResource csvResource = csvService.getCsvResource(ActivityLogSearchQuery.of(request));
+        final CsvResource2 csvResource = csvService.getCsvResource(ActivityLogSearchQuery.of(request));
+        relayS3Client.putObject(csvResource, csvNotifiableObjectMetadata(request.getEmail()));
     }
 }
