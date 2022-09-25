ccf7e08bdad4413411e0e2508bef4d671270fc97
yaboong <daniel.yebon.yoo@gmail.com>
2022-09-25 19:29:00 +0900

    [RA-77] refactor: s3 object metadata user metadata key 값 상수화

--- src/main/java/one/appscale/relayclientapi/infra/aws/s3/helper/ObjectMetadataFactory.java
+++ src/main/java/one/appscale/relayclientapi/infra/aws/s3/helper/ObjectMetadataFactory.java
@@ -5,10 +5,14 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import java.util.Map;
 
 public class ObjectMetadataFactory {
+    public static class UserMetadataKeys {
+        public static final String MAIL_TO = "mailTo";
+    }
+
     public static ObjectMetadata csvNotifiableObjectMetadata(final String mailTo) {
         final ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType("text/csv");
-        objectMetadata.setUserMetadata(Map.of("mailTo", mailTo));
+        objectMetadata.setUserMetadata(Map.of(UserMetadataKeys.MAIL_TO, mailTo));
         return objectMetadata;
     }
 }
