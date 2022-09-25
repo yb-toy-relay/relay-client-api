6300067cb050d73baa857b68a90a5da1434c227d
yaboong <daniel.yebon.yoo@gmail.com>
2022-09-25 20:29:44 +0900

    [RA-82] add: logging

--- src/main/java/one/appscale/relayclientapi/domain/csv/CsvNotificationService.java
+++ src/main/java/one/appscale/relayclientapi/domain/csv/CsvNotificationService.java
@@ -2,6 +2,7 @@ package one.appscale.relayclientapi.domain.csv;
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import one.appscale.relayclientapi.infra.aws.s3.RelayS3Client;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import java.net.URL;
 
 import static one.appscale.relayclientapi.infra.aws.s3.helper.ObjectMetadataFactory.UserMetadataKeys.MAIL_TO;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class CsvNotificationService {
@@ -17,9 +19,11 @@ public class CsvNotificationService {
     public String getEmailFromS3Object(final String objectKey) {
         final ObjectMetadata objectMetadata = s3Client.getObjectMetadata(objectKey);
         if (objectMetadata == null) {
+            log.warn("objectMetadata is null. key:{}", objectKey);
             return "";
         }
         if (objectMetadata.getUserMetadata() == null) {
+            log.warn("objectMetadata.userMetadata is null. key:{}", objectKey);
             return "";
         }
         return objectMetadata.getUserMetadata().get(MAIL_TO);
