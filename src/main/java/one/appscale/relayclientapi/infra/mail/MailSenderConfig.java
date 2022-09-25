e0cef01a5a1762231fc51fc64b80065ee3b2ab7f
yaboong <daniel.yebon.yoo@gmail.com>
2022-09-25 21:53:19 +0900

    [RA-87] add: 메일 발송 설정

---
+++ src/main/java/one/appscale/relayclientapi/infra/mail/MailSenderConfig.java
@@ -0,0 +1,33 @@
+package one.appscale.relayclientapi.infra.mail;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailSenderConfig {
+    @Value("${app.mail.user}")
+    private String user;
+
+    @Value("${app.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSenderImpl javaMailSender() {
+        final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(user);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
