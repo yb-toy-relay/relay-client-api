37664b923a032e025f903612949008c9ef14319f
yaboong <daniel.yebon.yoo@gmail.com>
2022-10-10 20:14:57 +0900

    [RA-113] add: swagger - api description (어노테이션으로 변경)

--- src/main/java/one/appscale/relayclientapi/common/config/SwaggerConfiguration.java
+++ src/main/java/one/appscale/relayclientapi/common/config/SwaggerConfiguration.java
@@ -1,18 +1,16 @@
 package one.appscale.relayclientapi.common.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "relay-client-api docs",
+        version = "v1",
+        description = "관리자용 API 만 포함"
+    )
+)
 public class SwaggerConfiguration {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info().title("relay-client-api docs")
-                            .description("관리자용 API 만 포함")
-                            .version("v1"))
-            ;
-    }
 }
