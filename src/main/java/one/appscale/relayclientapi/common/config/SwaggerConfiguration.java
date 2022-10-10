789908aeedadd0dfb1409198a1873d6e69a3f8cd
yaboong <daniel.yebon.yoo@gmail.com>
2022-10-10 21:33:33 +0900

    [RA-117] fix: swagger server url https

--- src/main/java/one/appscale/relayclientapi/common/config/SwaggerConfiguration.java
+++ src/main/java/one/appscale/relayclientapi/common/config/SwaggerConfiguration.java
@@ -2,10 +2,14 @@ package one.appscale.relayclientapi.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
+    servers = {
+        @Server(url = "/", description = "Default Server URL")
+    },
     info = @Info(
         title = "relay-client-api docs",
         version = "v1",
