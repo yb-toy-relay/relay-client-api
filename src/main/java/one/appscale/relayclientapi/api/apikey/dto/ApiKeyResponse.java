8d097a533565ea4c05402a116688802643cc58c0
yaboong <daniel.yebon.yoo@gmail.com>
2022-10-10 19:50:19 +0900

    [RA-112] add: 응답포맷에 eventTokens, emailDomains 추가

--- src/main/java/one/appscale/relayclientapi/api/apikey/dto/ApiKeyResponse.java
+++ src/main/java/one/appscale/relayclientapi/api/apikey/dto/ApiKeyResponse.java
@@ -6,10 +6,14 @@ import java.util.Set;
 
 public record ApiKeyResponse(String owner,
                              String key,
-                             Set<String> appTokens) {
+                             Set<String> appTokens,
+                             Set<String> eventTokens,
+                             Set<String> emailDomains) {
     public static ApiKeyResponse of(final ApiKeyDocument document) {
         return new ApiKeyResponse(document.getOwner(),
                                   document.getApiKey(),
-                                  document.getAppTokens());
+                                  document.getAppTokens(),
+                                  document.getEventTokens(),
+                                  document.getEmailDomains());
     }
 }
